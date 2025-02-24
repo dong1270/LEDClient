f58f8c36098cf18164e86909e57b3f358f8ec283
dong1270 <kang1270ds@gmail.com>
2025-02-24 19:16:26 +0900

    fix: socket thread

--- myClient.c
+++ myClient.c
@@ -13,48 +13,51 @@
 int main(int argc, char **argv)
 {
     int sock;
-    struct sockaddr_in serv_addr;
-    int buf[1024];
     int recv_len = 0;
     char *ret;
+    
+    struct sockaddr_in serv_addr;
+    
+    int buf[1024];
+    char id[100];
 
-    if(argc == 0) return (1);
+    if(argc != 2) return 0;
+
+    printf("%s\n", argv[1]);
 
     sock = socket(PF_INET, SOCK_STREAM, 0);
-    if(sock == -1)  return (1);
+    if(sock == -1) 
+    {
+        printf("socket() err\n ");
+        return (0);
+    }
 
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family        = AF_INET;
     serv_addr.sin_port          = htons(MY_PORT);
-    serv_addr.sin_addr.s_addr   = inet_addr("192.168.0.69");
+    serv_addr.sin_addr.s_addr   = inet_addr("127.0.0.1");
     
     if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) 
     {
-        return (1);
+        printf("connect() err\n");
+        return (0);
     }
 
+    // printf("send: %s\n", argv[1]);
     write(sock, argv[1], 15);
 
+    sleep(1);
     recv_len = read(sock, buf, 200);
 
-    if(recv_len == 0) return (1);
+    printf("test %d \n", recv_len);
+
+    if(recv_len == 0) printf("err\n");
     else{
         ret = calloc(recv_len, sizeof(char));
-
-        if(ret == NULL) 
-        {
-            close(sock);
-            return (0);
-        }
-
+        
         for(int i = 0; i < recv_len; i++) ret[i] = buf[i];
-        if(strcmp("e", ret) == 0) 
-        {
-            free(ret);
-            close(sock);
-            return (1);
-        }
-        free(ret);
+
+        printf("ret : %s\n", ret);
     }
 
     close(sock);
